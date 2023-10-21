2602cf9254614d4b44674247a9f795fa22eef62a
yousseffrk <youssef.mwalidfarouk@mail.mcgill.ca>
2023-10-21 00:27:09 -0400

    Added Todo Tests

--- PartA_Unit_Tests/src/test/java/BugFound.java
+++ PartA_Unit_Tests/src/test/java/BugFound.java
@@ -20,13 +20,22 @@ import static org.junit.jupiter.api.Assumptions.assumeFalse;
 public class BugFound {
 
     private int projectId;
+    private int todoId;
+
+    private final String TODO_TITLE = "Test Todo";
+    private final String TODO_DESCRIPTION = "Description of the test todo";
+
+    private final String UPDATED_TITLE = "New title";
+    private final String UPDATED_DESCRIPTION = "New description";
+
+    private boolean todoDeleted = false;
 
     @BeforeAll
     public static void initialSetup() {
         RestAssured.baseURI = "http://localhost:4567";
     }
 
-    public void creatProject() {
+    public void createProject() {
         // Create a project before each test
         Map<String, String> projectData = new HashMap<>();
         projectData.put("title", "Test Project");
@@ -44,9 +53,29 @@ public class BugFound {
         System.out.println("Set Up project with ID projectId");
     }
 
+
+    @BeforeEach
+    public void createTodo() {
+        // Create a todo before each test
+        Map<String, String> todoData = new HashMap<>();
+        todoData.put("title", TODO_TITLE);
+        todoData.put("description", TODO_DESCRIPTION);
+
+        Response response = given()
+                .contentType("application/json")
+                .body(todoData)
+                .when()
+                .post("/todos");
+        assertEquals(201, response.getStatusCode());
+
+        // Extract the project ID for further operations
+        todoId = response.jsonPath().getInt("id");
+        System.out.println("Set Up todo with ID " + todoId);
+    }
+
     @Test
     void testUpdateProjectWithStringValues() {
-        creatProject();
+        createProject();
         Map<String, String> projectData = new HashMap<>();
         projectData.put("completed", "true");
         projectData.put("active", "true");
@@ -64,4 +93,41 @@ public class BugFound {
         }
         assertEquals(200, updateResponse.getStatusCode());
     }
+
+    @Test
+    void testServerHeartbeat() {
+        createProject();
+        Response response = given()
+                .when()
+                .get("/heartbeat");
+        assertEquals(200, response.getStatusCode());
+    }
+
+    @Test
+    void testUpdateTodoUsingIdWithPut() {
+        Map<String, String> todoDataUpdated = new HashMap<>();
+        todoDataUpdated.put("description", UPDATED_DESCRIPTION);
+
+        Response response = given()
+                .pathParam("id", todoId)
+                .contentType("application/json")
+                .body(todoDataUpdated)
+                .when()
+                .put("/todos/{id}");
+        assertEquals(200, response.getStatusCode());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (!todoDeleted) {
+            // Delete the todo after each test
+            Response response = given()
+                    .pathParam("id", todoId)
+                    .when()
+                    .delete("/todos/{id}");
+            assertEquals(200, response.getStatusCode());
+        }
+        todoDeleted = false;
+    }
+
 }
